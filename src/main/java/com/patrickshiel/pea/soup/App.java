eaa1471b78059037b348c01562b5af2725165ae5
Patrick Shiel <paddyshiel@gmail.com>
2017-08-01 21:53:48 +1000

    Project Creation

---
+++ src/main/java/com/patrickshiel/pea/soup/App.java
@@ -0,0 +1,11 @@
+package com.patrickshiel.pea.soup;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+}
